1a33adee4e99eda3e29e4f48ede08b1f58d83ff5
arda.karayel <arda.karayel@sabanciuniv.edu>
2025-10-31 20:16:03 +0300

    Added cart module and updated users module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import { Cart } from './cart/cart.entity';
+import { CartItem } from './cart/cart-item.entity';
+import { CartModule } from './cart/cart.module';
 
 // Database connection configuration using TypeORM
 
@@ -19,7 +22,7 @@ import { ProductModule } from './product/product.module';
         username: 'root',
         password: '1234',
         database: 'onlinestore',
-        entities: [Product, User],
+        entities: [Product, User, Cart, CartItem],
         synchronize: true,
         logging: true,
       }),
@@ -33,6 +36,7 @@ import { ProductModule } from './product/product.module';
 
     // Importing the Product module (which contains all product CRUD logic)
     ProductModule,
+    CartModule,
     UsersModule,
     AuthModule,
   ],
